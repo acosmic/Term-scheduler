3cf024b26734b182087cf0b0d9d66f1960a46bbf
Clint Mireles <clintmireles@gmail.com>
2023-03-13 17:38:29 -0500

    Part 1 complete

---
+++ app/src/main/java/com/example/c196_app/ui/ClassDetails.java
@@ -0,0 +1,17 @@
+package com.example.c196_app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.c196_app.R;
+
+public class ClassDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_details);
+
+    }
+}
--- app/src/main/java/com/example/c196_app/ui/TermDetails.java
+++ app/src/main/java/com/example/c196_app/ui/TermDetails.java
@@ -1,26 +1,27 @@
 package com.example.c196_app.ui;
 
+import static com.example.c196_app.R.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.c196_app.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class TermList extends AppCompatActivity {
+public class TermDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_term_list);
-        FloatingActionButton fab=findViewById(R.id.addTermButton);
+        setContentView(layout.activity_term_details);
+        FloatingActionButton fab=findViewById(id.addClassDetails);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                Intent intent=new Intent(TermList.this, TermDetails.class);
+                Intent intent=new Intent(TermDetails.this, ClassDetails.class);
                 startActivity(intent);
             }
         });
