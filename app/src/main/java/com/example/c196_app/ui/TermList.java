f80ea5b675b6e099a0735a990c69271c581fbcb9
Clint Mireles <clintmireles@gmail.com>
2023-03-13 17:38:29 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/c196_app/ui/TermList.java
@@ -0,0 +1,16 @@
+package com.example.c196_app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.c196_app.R;
+
+public class TermList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+    }
+}
