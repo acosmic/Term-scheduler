93a7830bf653d6c61d3ab5848aa0e0e6d216986c
Clint <clintmireles@gmail.com>
2023-07-04 21:48:35 -0500

    Added User table and entity
    Finished Entity
    
    Need to work on login screen

--- app/src/main/java/com/example/c196_app/entities/User.java
+++ app/src/main/java/com/example/c196_app/entities/User.java
@@ -10,4 +10,35 @@ public class User {
     private String userName;
     private String passWord;
 
+    public User(int ID, String userName, String passWord) {
+        this.ID = ID;
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+
+    public User() {}
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
 }
