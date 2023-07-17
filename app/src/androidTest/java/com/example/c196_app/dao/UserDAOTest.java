7ed07e64e23f3f0a84de39e4314d6d7c5b325630
Clint <clintmireles@gmail.com>
2023-07-16 20:17:19 -0500

    insert and delete tests working
    Need to fix update test, then make same tests for Terms and Courses

--- app/src/androidTest/java/com/example/c196_app/dao/UserDAOTest.java
+++ app/src/androidTest/java/com/example/c196_app/dao/UserDAOTest.java
@@ -12,18 +12,22 @@ import java.util.List;
 @RunWith(AndroidJUnit4.class)
 public class UserDAOTest{
     boolean userExists;
-
+    boolean userUpdated;
+    Repository repository = new Repository(ApplicationProvider.getApplicationContext());
+    
+    User testDeleteUser;
 
     @Test
     public void insertAndReturnUser() {
-        Repository repository = new Repository(ApplicationProvider.getApplicationContext());
+        // working
+        userExists = false;
         User testUser = new User(0,"Test", "Testing@123");
         repository.insert(testUser);
         List<User> userList = repository.getAllUsers();
-        userExists = false;
         for (User user : userList) {
             if (user.getUserName().equals(testUser.getUserName()) && user.getPassWord().equals(testUser.getPassWord())) {
                 userExists = true;
+                repository.delete(user);
                 break;
             }
         }
@@ -31,14 +35,51 @@ public class UserDAOTest{
     }
 
     @Test
-    public void update() {
+    public void insertAndUpdateUser() {
+        userUpdated = false;
+        int testUpdateUserID = 0;
+        User testUser = new User(0, "Test", "Testing@123");
+        repository.insert(testUser);
+        List<User> userList = repository.getAllUsers();
+        for (User user : userList) {
+            if (user.getUserName().equals(testUser.getUserName()) && user.getPassWord().equals(testUser.getPassWord())) {
+                testUpdateUserID = user.getID();
+                break;
+            }
+        }
+        User testUpdateUser = new User(testUpdateUserID, "testing", "123@Testing");
+        repository.update(testUpdateUser);
+
+        // NOT WORKING - WHAT THE FUCK IS WRONG WITH THIS
+        List<User> updatedUserList = repository.getAllUsers();
+        for (User user : updatedUserList) {
+            if (user.getUserName().equals("testing") && user.getPassWord().equals("123@Testing")) {
+                userUpdated = true;
+                repository.delete(user);
+                break;
+            }
+        }
+        assertTrue(userUpdated);
     }
 
     @Test
-    public void delete() {
-    }
-
-    @Test
-    public void getAllUsers() {
+    public void insertAndDeleteUser() {
+        // THIS BULLSHIT WORKS THANK FUCK
+        userExists = false;
+        User testUser = new User(0,"Test", "Testing@123");
+        repository.insert(testUser);
+        List<User> userList = repository.getAllUsers();
+        for (User user : userList) {
+            if (user.getUserName().equals(testUser.getUserName()) && user.getPassWord().equals(testUser.getPassWord())) {
+                userExists = true;
+                repository.delete(user);
+                break;
+            }
+        }
+        List<User> userUpdatedList = repository.getAllUsers();
+        if (userUpdatedList.isEmpty()) {
+            userExists = false;
+        }
+        assertFalse(userExists);
     }
 }
